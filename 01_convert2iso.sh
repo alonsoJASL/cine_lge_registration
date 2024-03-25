caac78e3cbb37b07a4cc704aa12da5bbe7b8aaba
Jose Alonso Solis Lemus <jsolisle@ic.ac.uk>
2024-03-25 16:27:58 +0000

    reorient

--- 01_convert2iso.sh
+++ 01_convert2iso.sh
@@ -10,8 +10,15 @@ DATA=$1
 OUTPUT=$2 
 INTERP=$3 
 
+echo "Resampling $DATA to $OUTPUT with $INTERP interpolation"
 CODE="/home/jsl19/installs/mirtk.base/build/bin"
-
 $CODE/mirtk resample-image $DATA $OUTPUT -size 1 1 1 -interp $INTERP
+
+# now reorient to RAI 
+
+echo "Reorienting $OUTPUT to RAI"
+CEMRG="/home/jsl19/dev/build/CEMRG2-U20.04/MITK-build/bin"
+$CEMRG/MitkCemrgResampleReorient -i $OUTPUT -o $OUTPUT --reorient
+
 
 
